b63ef01ac4d3234e4ddecac84dd38804e8d55a9f
morris88826 <morris88826@gmail.com>
2024-11-28 13:21:49 -0600

    overall structure

---
+++ website/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <nav>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/about">About</router-link></li>
+        <li><router-link to="/contact">Contact</router-link></li>
+      </ul>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<style>
+nav {
+  background-color: #333;
+  padding: 1rem;
+}
+ul {
+  list-style: none;
+  display: flex;
+  gap: 1rem;
+  margin: 0;
+  padding: 0;
+}
+li {
+  display: inline;
+}
+a {
+  color: white;
+  text-decoration: none;
+}
+a:hover {
+  text-decoration: underline;
+}
+</style>
