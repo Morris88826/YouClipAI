1ba73f8d458b3e299201c8448ae7f4835b75c846
morris88826 <morris88826@gmail.com>
2024-12-02 14:17:18 -0600

    connect asr to frontend

--- backend/api/video_routes.py
+++ backend/api/video_routes.py
@@ -1,23 +1,29 @@
 import os
 import time
+import json
+import whisper
 from threading import Thread
 from pytubefix import YouTube
 from moviepy.video.io.VideoFileClip  import VideoFileClip
 from flask import Blueprint, request, jsonify
 from init import overview_chain, searcher
+from pydub import AudioSegment
+from pydub.utils import make_chunks
 
 video_bp  = Blueprint('video_routes', __name__, url_prefix='/api/videos')
-video_out_dir = './downloads'
+download_dir='./downloads'
+asr_model = whisper.load_model("turbo")
 
 tasks = {}
-@video_bp.route('/analyze', methods=['POST'])
-def analyze_video():
+@video_bp.route('/fetch', methods=['POST'])
+def fetch_video():
     # Logic for analyzing a video
     data = request.get_json()
     youtube_url = data.get('youtubeURL')
 
     task_id = str(int(time.time()))
     tasks[task_id] = {
+        "task_type": "fetch_video",
         "status": "processing",
         "progress": 0,
         "message": "Processing the video",
@@ -28,57 +34,170 @@ def analyze_video():
         }
     }
     # Start the background process
-    thread = Thread(target=process_video, args=(youtube_url, task_id))
+    thread = Thread(target=_fetch_video, args=(youtube_url, task_id))
     thread.start()
 
     return jsonify({"status": "success", "task_id": task_id})
 
-def process_video(youtube_url, task_id):
+def _fetch_video(youtube_url, task_id):
     try:
         yt = YouTube(youtube_url)
+        video_url = yt.streams.first().url
+        video_id = yt.video_id
         video_title = yt.title
 
-        stream = yt.streams.first()
-        video_out_path = stream.download(video_out_dir)
+        video_out_dir = os.path.join(download_dir, video_id)
+        if os.path.exists(video_out_dir):
+            metdata = os.path.join(video_out_dir, 'metadata.json')
+            if os.path.exists(metdata):
+                with open(metdata, 'r') as f:
+                    metadata = json.load(f)
+                tasks[task_id] = {
+                    "task_type": "fetch_video",
+                    "status": "completed",
+                    "progress": 100,
+                    "message": "Successfully load the video",
+                    "video": {
+                        "title": metadata["title"],
+                        "id": metadata["id"],
+                        "url": metadata["url"]
+                    }
+                }
+                return
 
-        audio_out_dir = os.path.join(video_out_dir, 'audio')
-        os.makedirs(audio_out_dir, exist_ok=True)
-        audio_out_path = os.path.join(audio_out_dir, f'{video_title}.wav')
+        os.makedirs(video_out_dir, exist_ok=True)
+
+        stream = yt.streams.first()
+        video_out_path = stream.download(video_out_dir, filename='raw_video.mp4')
+
+        audio_out_path = os.path.join(video_out_dir, f'raw_audio.wav')
         video = VideoFileClip(video_out_path)
         audio = video.audio
         audio.write_audiofile(audio_out_path)
         video.close()
         audio.close()
 
+        metadata = {
+            "title": video_title,
+            "id": video_id,
+            "url": video_url
+        }
+
         tasks[task_id] = {
             "status": "completed",
             "progress": 100,
             "message": "Successfully processed the video",
+            "video": metadata
+        }
+
+
+        with open(os.path.join(video_out_dir, 'metadata.json'), 'w') as f:
+            json.dump(metadata, f)
+
+    except Exception as e:
+        tasks[task_id] = {
+            "status": "error",
+            "progress": 100,
+            "message": str(e),
             "video": {
-                "title": video_title,
-                "id": yt.video_id,
+                "title": "",
+                "id": "",
                 "url": youtube_url
             }
         }
-    except Exception as e:
-        print(e)
     return 
 
 @video_bp.route('/progress/<task_id>', methods=['GET'])
 def progress(task_id):
     task = tasks.get(task_id)
     if not task:
+        tasks.pop(task_id)
         return jsonify({"status": "error", "message": "Task not found"}), 404
-    return jsonify({
-        "status": task["status"],
-        "progress": task["progress"],
-        "message": task["message"],
-        "video": {
-            "title": task["video"]["title"],
-            "id": task["video"]["id"],
-            "url": task["video"]["url"]
+    
+    if task['task_type'] == 'fetch_video':
+        p = jsonify({
+            "status": task["status"],
+            "task_type": task["task_type"],
+            "progress": task["progress"],
+            "message": task["message"],
+            "video": {
+                "title": task["video"]["title"],
+                "id": task["video"]["id"],
+                "url": task["video"]["url"]
+            }
+        })
+    elif task['task_type'] == 'analyze_asr':
+        p = jsonify({
+            "status": task["status"],
+            "task_type": task["task_type"],
+            "progress": task["progress"],
+            "message": task["message"],
+        })
+
+    if task["status"] == "completed":
+        tasks.pop(task_id)
+    return p
+
+@video_bp.route('/analyze_asr', methods=['POST'])
+def analyze_asr():
+    data = request.get_json()
+    video_id = data.get('video_id')
+    video_out_dir = os.path.join(download_dir, video_id)
+    if not os.path.exists(video_out_dir):
+        return jsonify({"status": "error", "message": "Video not found"}), 404
+    
+    task_id = str(int(time.time()))
+    tasks[task_id] = {
+        "task_type": "analyze_asr",
+        "status": "processing",
+        "progress": 0,
+        "message": "Processing the audio",
+    }
+    # Start the background process
+    thread = Thread(target=_analyze_asr, args=(video_id, task_id))
+    thread.start()
+    return jsonify({"status": "success", "task_id": task_id})
+    
+def _analyze_asr(video_id, task_id, chunk_length_ms=120*1000):
+    video_out_dir = os.path.join(download_dir, video_id)
+    audio_out_path = os.path.join(video_out_dir, 'raw_audio.wav')
+    audio_out_chunk_dir = os.path.join(video_out_dir, 'chunks')
+    transcription_out_dir = os.path.join(video_out_dir, 'transcriptions')
+    os.makedirs(audio_out_chunk_dir, exist_ok=True)
+    os.makedirs(transcription_out_dir, exist_ok=True)
+    try:
+        audio = AudioSegment.from_file(audio_out_path)
+        chunks = make_chunks(audio, chunk_length_ms)
+
+        progress_step = int(100 / len(chunks))
+        for i, chunk in enumerate(chunks):
+            # Save the chunk to disk
+            audio_chunk_path = os.path.join(audio_out_chunk_dir, f"{i:04d}.wav")
+            if not os.path.exists(audio_chunk_path):
+                chunk.export(audio_chunk_path, format="wav")
+            # Transcribe with Whisper
+            transcription_out_path = os.path.join(transcription_out_dir, f"{i:04d}.txt")
+            if not os.path.exists(transcription_out_path):
+                result = asr_model.transcribe(audio_chunk_path)
+                with open(transcription_out_path, 'w') as f:
+                    f.write(result['text'])
+            tasks[task_id]['progress'] += progress_step
+
+        tasks[task_id] = {
+            "task_type": "analyze_asr",
+            "status": "completed",
+            "progress": 100,
+            "message": "Successfully processed the audio",
         }
-    })
+    except Exception as e:
+        tasks[task_id] = {
+            "task_type": "analyze_asr",
+            "status": "error",
+            "progress": 100,
+            "message": str(e),
+        }
+    return
+
 
 @video_bp.route('/search', methods=['POST'])
 def search():
--- website/src/views/QuickStartView.vue
+++ website/src/views/QuickStartView.vue
@@ -2,7 +2,9 @@
   <div id="app" class="container mt-5">
     <div class="text-center mb-4">
       <h1 class="display-4">YouClipAI - Quick Start</h1>
-      <p class="lead">Analyze and extract information from YouTube videos easily.</p>
+      <p class="lead">
+        Analyze and extract information from YouTube videos easily.
+      </p>
     </div>
 
     <!-- Search Form -->
@@ -21,8 +23,13 @@
           @click="submitQuery"
           :disabled="loading"
         >
-          <span v-if="loading" class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
-          {{ loading ? " Processing..." : "Analyze Video" }}
+          <span
+            v-if="loading"
+            class="spinner-border spinner-border-sm"
+            role="status"
+            aria-hidden="true"
+          ></span>
+          {{ loading ? " Processing..." : "Fetch Video" }}
         </button>
       </div>
     </div>
@@ -35,51 +42,42 @@
       <p class="mt-2">Processing your request, please wait...</p>
     </div>
 
-    <!-- Progress Bar -->
-    <div v-if="progress > 0 && progress < 100" class="mt-4">
-      <div class="progress" style="height: 30px;">
-        <div
-          class="progress-bar progress-bar-striped progress-bar-animated bg-success"
-          role="progressbar"
-          :style="{ width: `${progress}%` }"
-          :aria-valuenow="progress"
-          aria-valuemin="0"
-          aria-valuemax="100"
-        >
-          {{ progress }}%
-        </div>
-      </div>
-    </div>
-
-    <!-- Transcriptions -->
-    <div v-if="transcriptions.length > 0" class="mt-5">
-      <h3>Transcriptions:</h3>
-      <div class="card mt-3">
-        <div class="card-body">
-          <ul class="list-group list-group-flush">
-            <li v-for="(transcription, index) in transcriptions" :key="index" class="list-group-item">
-              <p class="mb-1"><strong>Part {{ index + 1 }}:</strong></p>
-              <p class="mb-0">{{ transcription }}</p>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-
     <!-- Search Result -->
     <div v-if="video" class="mt-5">
       <h3>Video Search Result:</h3>
       <div class="card mt-3">
         <div class="card-body">
           <h5 class="card-title">{{ video.title }}</h5>
-          <a
-            :href="`${video.url}`"
+          <iframe
+            :src="`${video.url.replace('watch?v=', 'embed/')}`"
+            width="100%"
+            height="315"
+            frameborder="0"
+            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+          <button
             class="btn btn-secondary mt-3"
-            target="_blank"
-            rel="noopener noreferrer"
+            style="min-width: 0"
+            @click="startAnalysis"
           >
-            Watch on YouTube
-          </a>
+            Analyze
+          </button>
+
+          <div v-if="progress > 0 && progress <= 100" class="mt-4">
+            <div class="progress" style="height: 30px">
+              <div
+                class="progress-bar progress-bar-striped progress-bar-animated bg-info"
+                role="progressbar"
+                :style="{ width: `${progress}%` }"
+                :aria-valuenow="progress"
+                aria-valuemin="0"
+                aria-valuemax="100"
+              >
+                {{ progress }}%
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -92,7 +90,8 @@ import axios from "axios";
 export default {
   data() {
     return {
-      youtubeURL: "",
+      youtubeURL:
+        "https://www.youtube.com/watch?v=cNXxqE7hs9U&ab_channel=TheSportingTribune",
       progress: 0,
       loading: false,
       video: null,
@@ -113,9 +112,12 @@ export default {
 
       try {
         // Send query to the backend
-        const response = await axios.post(`${this.apiBaseUrl}/api/videos/analyze`, {
-          youtubeURL: this.youtubeURL,
-        });
+        const response = await axios.post(
+          `${this.apiBaseUrl}/api/videos/fetch`,
+          {
+            youtubeURL: this.youtubeURL,
+          }
+        );
 
         if (response.data.status === "success") {
           const taskId = response.data.task_id;
@@ -134,10 +136,33 @@ export default {
     async pollProgress(taskId) {
       const interval = setInterval(async () => {
         try {
-          const progressResponse = await axios.get(`${this.apiBaseUrl}/api/videos/progress/${taskId}`);
-          this.progress = progressResponse.data.progress;
-          this.video = progressResponse.data.video || {};
+          const progressResponse = await axios.get(
+            `${this.apiBaseUrl}/api/videos/progress/${taskId}`
+          );
 
+          const progress_type = progressResponse.data.task_type;
+          if (progressResponse.data.status === "error") {
+            clearInterval(interval);
+            this.loading = false;
+            this.progress = 0;
+            if (progress_type == "fetch_video"){
+              alert(
+                "Error processing the video. Please input a valid YouTube URL."
+              );
+            }
+            else if (progress_type == "analyze_asr"){
+              alert(
+                "Error processing the video. Please try again later."
+              );
+            }
+            return;
+          }
+          
+          this.progress = progressResponse.data.progress;
+          if (progress_type == "fetch_video")
+            this.video = progressResponse.data.video || {};
+          else if(progress_type == "analyze_asr")
+            this.transcriptions = progressResponse.data.transcriptions || [];
           // Show progress only when greater than 0
           if (this.progress > 0) {
             this.loading = false;
@@ -146,7 +171,7 @@ export default {
           if (this.progress >= 100) {
             clearInterval(interval);
             this.loading = false;
-            this.transcriptions = progressResponse.data.transcriptions || [];
+            this.progress = 0;
           }
         } catch (error) {
           console.error("Error polling progress:", error);
@@ -155,6 +180,32 @@ export default {
           alert("Error polling progress.");
         }
       }, 1000); // Poll every second
+    },
+
+    async startAnalysis() {
+      this.analyzeProgress = 0;
+
+      try {
+        // Send query to the backend
+        const response = await axios.post(
+          `${this.apiBaseUrl}/api/videos/analyze_asr`,
+          {
+            video_id: this.video.id,
+          }
+        );
+
+        if (response.data.status === "success") {
+          const taskId = response.data.task_id;
+          this.pollProgress(taskId);
+        } else {
+          alert("Error starting the process.");
+          this.loading = false;
+        }
+      } catch (error) {
+        console.error("Error:", error);
+        alert("Failed to send query.");
+        this.loading = false;
+      }
     },
   },
 };
